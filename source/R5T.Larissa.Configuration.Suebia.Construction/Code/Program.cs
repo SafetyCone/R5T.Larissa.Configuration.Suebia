28349a550acdae134cc8bc67314688f29f9d3b0a
MinexAutomation <minex.automation@gmail.com>
2019-12-21 01:13:09 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Larissa.Configuration.Suebia.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Larissa.Configuration.Suebia.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Larissa.Configuration.Suebia/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Larissa.Configuration.Suebia
+{
+    public class Class1
+    {
+    }
+}
